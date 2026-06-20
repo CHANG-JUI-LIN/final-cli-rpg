1ed1a74ed82a1559c8b3731380ada542fb60eee7
sodaoao <henry9509072@gmail.com>
2026-06-20 17:56:55 +0800

    Update Player.h

--- src/Player.h
+++ src/Player.h
@@ -1,27 +1,52 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include<iostream>
-#include<string>
+#include <string>
+#include <vector>
+#include "Item.h"
+#include "Skill.h"
 
-class Player
-{
+using namespace std;
+
+class Player {
 private:
-    std::string name;
+    string name;
     int hp;
+    int maxHp;
+    int mp;
+    int maxMp;
     int attackPower;
-    std::string items[];
+    int gold;
+    vector<Item> items;
+    vector<Skill> skills;
+
 public:
     Player();
-    ~Player();
+    Player(string name, int hp, int mp, int attackPower);
 
     int attack();
-    int takeDamage();
-    void useItem();
+    int useSkill(int index);
+    void takeDamage(int damage);
+    void useItem(int index);
     bool isAlive();
+
+    void addItem(Item item);
+    void addSkill(Skill skill);
+    void showStatus();
+    void showItems();
+    void showSkills();
+    void gainGold(int amount);
+
+    string getName();
+    int getHp();
+    int getMp();
+    int getAttackPower();
+    int getGold();
+
+    void setName(string name);
+    void setHp(int hp);
+    void setMp(int mp);
+    void setAttackPower(int attackPower);
 };
 
-
-
-
-#endif PLAYER_H
+#endif
