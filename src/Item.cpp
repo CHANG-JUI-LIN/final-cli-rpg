39bee17f353b7ed986cd69282eaa820de7af8717
sodaoao <henry9509072@gmail.com>
2026-06-20 17:58:24 +0800

    Update Item.cpp

--- src/Item.cpp
+++ src/Item.cpp
@@ -1,22 +1,73 @@
 #include "Item.h"
 #include <iostream>
 
+using namespace std;
 
-Item::Item(/* args */)
-{
+Item::Item() {
+    name = "Potion";
+    type = "heal";
+    effectValue = 20;
+    quantity = 1;
 }
 
-Item::~Item()
-{
+Item::Item(string name, string type, int effectValue, int quantity) {
+    this->name = name;
+    this->type = type;
+    this->effectValue = effectValue;
+    this->quantity = quantity;
 }
 
-bool Item::isAvailable(){
+int Item::use() {
+    if (!isAvailable()) {
+        cout << "No item left." << endl;
+        return 0;
+    }
 
+    quantity--;
+    cout << "Used item: " << name << endl;
+    return effectValue;
 }
-void Item::showInfo(){
 
+void Item::showInfo() {
+    cout << name
+         << " | Type: " << type
+         << " | Effect: " << effectValue
+         << " | Quantity: " << quantity
+         << endl;
 }
 
-void Item::use(){
+bool Item::isAvailable() {
+    return quantity > 0;
+}
 
+string Item::getName() {
+    return name;
+}
+
+string Item::getType() {
+    return type;
+}
+
+int Item::getEffectValue() {
+    return effectValue;
+}
+
+int Item::getQuantity() {
+    return quantity;
+}
+
+void Item::setName(string name) {
+    this->name = name;
+}
+
+void Item::setType(string type) {
+    this->type = type;
+}
+
+void Item::setEffectValue(int effectValue) {
+    this->effectValue = effectValue;
+}
+
+void Item::setQuantity(int quantity) {
+    this->quantity = quantity;
 }
