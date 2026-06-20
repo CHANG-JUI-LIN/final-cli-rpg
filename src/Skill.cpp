5d7cfaf9ac03b887e01118ce941e52cab71f8a16
sodaoao <henry9509072@gmail.com>
2026-06-20 17:57:58 +0800

    Update Skill.cpp

--- src/Skill.cpp
+++ src/Skill.cpp
@@ -1,21 +1,53 @@
 #include "Skill.h"
-#include<iostream>
+#include <iostream>
 
-Skill::Skill(/* args */)
-{
+using namespace std;
+
+Skill::Skill() {
+    name = "Basic Skill";
+    damage = 10;
+    mpCost = 5;
 }
 
-Skill::~Skill()
-{
+Skill::Skill(string name, int damage, int mpCost) {
+    this->name = name;
+    this->damage = damage;
+    this->mpCost = mpCost;
 }
 
-bool Skill::use(){
-    
+int Skill::use() {
+    cout << "Used skill: " << name << endl;
+    cout << "Skill damage: " << damage << endl;
+    return damage;
 }
 
-void Skill::showInfo(){
-    
+void Skill::showInfo() {
+    cout << name
+         << " | Damage: " << damage
+         << " | MP Cost: " << mpCost
+         << endl;
 }
-int Skill::getDamage(){
 
+int Skill::getDamage() {
+    return damage;
+}
+
+string Skill::getName() {
+    return name;
+}
+
+int Skill::getMpCost() {
+    return mpCost;
+}
+
+void Skill::setName(string name) {
+    this->name = name;
+}
+
+void Skill::setDamage(int damage) {
+    this->damage = damage;
+}
+
+void Skill::setMpCost(int mpCost) {
+    this->mpCost = mpCost;
 }
