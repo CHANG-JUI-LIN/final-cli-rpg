25fe594c0f72eb538e42179666208ea7d4e1cc78
ianis66666 <st062689985@gmail.com>
2026-06-21 05:33:39 +0800

    Polish Item.h

--- src/Item.h
+++ src/Item.h
@@ -7,26 +7,39 @@ using namespace std;
 
 class Item {
 private:
+    // Item name shown in the inventory.
     string name;
+
+    // Item type controls how Player::useItem applies the effect.
+    // Supported values: heal, mp, attack.
     string type;
+
+    // Amount of HP, MP, or attack value changed by this item.
     int effectValue;
+
+    // Number of times this item can still be used.
     int quantity;
 
 public:
     Item();
-    Item(string name, string type, int effectValue, int quantity);
+    Item(const string& name, const string& type, int effectValue, int quantity);
 
+    // Consume one item and return its effect value.
     int use();
-    void showInfo();
-    bool isAvailable();
 
-    string getName();
-    string getType();
-    int getEffectValue();
-    int getQuantity();
+    // Display item information in the item menu.
+    void showInfo() const;
 
-    void setName(string name);
-    void setType(string type);
+    // Check whether the item still has quantity left.
+    bool isAvailable() const;
+
+    string getName() const;
+    string getType() const;
+    int getEffectValue() const;
+    int getQuantity() const;
+
+    void setName(const string& name);
+    void setType(const string& type);
     void setEffectValue(int effectValue);
     void setQuantity(int quantity);
 };
