7dd060f37ccaa7825bc06d966b03ff2b3df8a496
ianis66666 <st062689985@gmail.com>
2026-06-21 05:32:55 +0800

    Polish Player.h

--- src/Player.h
+++ src/Player.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+
 #include "Item.h"
 #include "Skill.h"
 
@@ -10,6 +11,7 @@ using namespace std;
 
 class Player {
 private:
+    // Basic player status.
     string name;
     int hp;
     int maxHp;
@@ -17,33 +19,44 @@ private:
     int maxMp;
     int attackPower;
     int gold;
+
+    // Player inventory and learned skills.
     vector<Item> items;
     vector<Skill> skills;
 
 public:
     Player();
-    Player(string name, int hp, int mp, int attackPower);
+    Player(const string& name, int hp, int mp, int attackPower);
 
-    int attack();
+    // Return normal attack damage.
+    int attack() const;
+
+    // Use a skill by index and return the damage dealt.
     int useSkill(int index);
-    void takeDamage(int damage);
-    void useItem(int index);
-    bool isAlive();
 
-    void addItem(Item item);
-    void addSkill(Skill skill);
-    void showStatus();
-    void showItems();
-    void showSkills();
+    // Reduce player HP after a monster attack.
+    void takeDamage(int damage);
+
+    // Use an item by index and apply its effect to the player.
+    void useItem(int index);
+
+    // Check whether the player can continue fighting.
+    bool isAlive() const;
+
+    void addItem(const Item& item);
+    void addSkill(const Skill& skill);
+    void showStatus() const;
+    void showItems() const;
+    void showSkills() const;
     void gainGold(int amount);
 
-    string getName();
-    int getHp();
-    int getMp();
-    int getAttackPower();
-    int getGold();
+    string getName() const;
+    int getHp() const;
+    int getMp() const;
+    int getAttackPower() const;
+    int getGold() const;
 
-    void setName(string name);
+    void setName(const string& name);
     void setHp(int hp);
     void setMp(int mp);
     void setAttackPower(int attackPower);
